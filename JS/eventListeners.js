fcc9d43d7c0ee0403da7187c18a7e13aeab56328
Essa <essa992@gmail.com>
2020-02-13 11:35:35 +0100

    just showed how to use eventlisteners

--- JS/eventListeners.js
+++ JS/eventListeners.js
@@ -1,3 +1,11 @@
 export function addEventListener() {
     // You write here all your event listners
+    //     document.getElementById("users").onclick = showmeusers()
+    //     document.getElementById("users").addEventListener("click", function showMeUsers() {
+
+    //     })
+    // }
+    // document.getElementById("users").addEventListener("click", showMeUsers())
+    // function showmeusers() {
+
 }
