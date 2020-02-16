b1222be39023595ea659d85733265e891b3958d3
Essa <essa992@gmail.com>
2020-02-16 14:10:06 +0100

    --

--- JS/adminJS.js
+++ JS/adminJS.js
@@ -4,10 +4,14 @@ export function getUsers() {
     myData.append("entity", "user");
     myData.append("endpoint", "getAllUsers");
     makeRequest("../api/recivers/userReciver.php", "GET", null, (result) => {
+        let usersDiv = document.getElementById("usersDiv")
 
         for (var i = 0; i < result.length; i++) {
             console.log(result[i]);
-            document.getElementById("usersDiv").innerHTML = result[i];
+            usersDiv.innerHTML = result[i]
+            // let userDiv = document.createElement("div")
+            //     userDiv.innerHTML = result[i];
+            //     usersDiv.appendChild("userDiv")
         }
     })
 }
