2c523586ed1b2a2041884ba85913ce506469abd8
Anukhatra <anu.khatra@gmail.com>
2020-02-16 15:02:26 +0100

    eventHandler

--- JS/eventListeners.js
+++ JS/eventListeners.js
@@ -1,11 +1,4 @@
-export function addEventListener() {
-    // You write here all your event listners
-    //     document.getElementById("users").onclick = showmeusers()
-    //     document.getElementById("users").addEventListener("click", function showMeUsers() {
+import { signUpSubmit } from "./signupJS.js"
 
-    //     })
-    // }
-    // document.getElementById("users").addEventListener("click", showMeUsers())
-    // function showmeusers() {
+document.getElementById("signUpSubmit").addEventListener("click", signUpSubmit);
 
-}
