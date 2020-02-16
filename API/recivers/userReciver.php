941ec4ed78599a45ff63306ca9fa635b262f81a8
Anukhatra <anu.khatra@gmail.com>
2020-02-16 14:49:23 +0100

    reciveruser

---
+++ API/recivers/userReciver.php
@@ -0,0 +1,32 @@
+<?php
+include ("../handlers/userHandler.php");
+try {
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if ($_POST["entity"] == "enjoy") {
+            
+                if ($_POST["endpoint"] == "addSignUpUser") {
+               /*      $firstname = $_POST['firstname'];
+                    $lastname = $_POST['lastname'];
+                    $email = $_POST['email'];
+                    $role = $_POST['role'];
+                */     $firstname = "x";
+                    $lastname = "y";
+                    $email = "z";
+                    $role = "c";
+                   signUpSubmit($_POST["firstname"], $_POST["lastname"],$_POST["email"],$_POST["role"]);
+
+            } else {
+                throw new Exception("Not a valid endpoint", 501);
+            }
+        } else {
+            throw new Exception("Not a valid entity method", 501);
+        }
+    } else {
+        throw new Exception("Not valid request method", 405);
+    }
+} catch (Exception $e) {
+    echo json_encode(array("Message" => $e->getMessage(), "Status" => $e->getCode()));
+}
+
+?>
