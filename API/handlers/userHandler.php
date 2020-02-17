e34159e8ee3c3679daf70c53dec3d6e621dd5583
Baraabd <bara.abd@yahoo.com>
2020-02-17 18:35:41 +0100

    userHandler.php

--- API/handlers/userHandler.php
+++ API/handlers/userHandler.php
@@ -1,44 +1,10 @@
 <?php
+include("../classes/userClass.php");
+include("../classes/dbClass.php");
 
-function addUser($newUser) {
-$database = new Database();
-$sql = "INSERT INTO enjoy (firstname, lastname, email, role) VALUES (:firstname, :lastname, :email, :role)";
-$stmt = $database->connection->prepare($sql);
-$stmt->execute(json_decode($newUser, true));
-$result = $sql;
-if (empty($result)) {
-    throw new Exception("No students found", 404);
-    exit;
-}
-return $result;
-}
-
-function updateUser() {
-$database = array ('firstname' => 'Susan Johnson');
-$sql = ("UPDATE enjoy SET firstname = :firstname WHERE id > 5");
-$stmt = $database->connection->prepare($sql);
-$stmt->execute($database);
-$result = $sql;
-if (empty($result)) {
-    throw new Exception("No students found", 404);
-    exit;
-}
-return $result;
-}
-
-function deleteUser() {
-
-$database = "";
-$sql = ("DELETE FROM enjoy WHERE id > 5");
-$stmt = $database->connection->prepare($sql);
-$stmt->execute();
-$result = $sql;
-
-if (empty($result)) {
-    throw new Exception("No students found", 404);
-    exit;
-}
-return $result;
-}
-
+function getUsers() {
+    $User = new User(null, null, null, null);
+    $result = $User->fetchAll("SELECT * FROM Users;");
+    echo json_encode($result );
+};
 ?>
