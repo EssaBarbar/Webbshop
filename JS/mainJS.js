edc7efb6378a6b7e8424e84609c1df8df5b62881
Baraabd <bara.abd@yahoo.com>
2020-02-17 18:50:56 +0100

    main.JS.js

--- JS/mainJS.js
+++ JS/mainJS.js
@@ -1,25 +1,16 @@
 window.onload = init()
 
-export function makeRequest(url, method, formData, callback) {
-    fetch(url, {
-        method: method,
-        body: formData
-    }).then((response) => {
-        console.log(response)
-        return response.json()
-    }).then((result) => {
-        callback(result)
-    }).catch((err) => {
-        console.log("Error : ", err);
-    })
+import { addEventListener } from "./eventListeners.js"
+import { getCategories } from "./categoryJS.js"
+import { getProducts } from "./productJS.js"
+import { signUp } from "./loginJS.js"
+
+function init() {
+    addEventListener()
+        // You write all the funcs that gonna be done when you load the login site here
+    getCategories()
+    getProducts()
+        /*    signUp() */
 }
-
-// function init() {
-//     //import { addEventListener } from "./eventListeners"
-//     //addEventListener()
-
-//     // You write all the funcs that gonna be done when you load the login site here
-
-// }
-// // you write all other stuffs the you want to do from other js pages here but you need to import them
-// // Like maybe doing one function that gonna controll every thins with the login and call it from here
+// you write all other stuffs the you want to do from other js pages here but you need to import them
+// Like maybe doing one function that gonna controll every thins with the login and call it from here
