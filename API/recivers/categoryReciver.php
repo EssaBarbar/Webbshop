795e2c3a160525b3c183908fa587fcc534e24fe9
Baraabd <bara.abd@yahoo.com>
2020-02-17 18:36:50 +0100

    categoryReciver.php

--- API/recivers/categoryReciver.php
+++ API/recivers/categoryReciver.php
@@ -1,35 +1,9 @@
 <?php
-echo json_encode("Hello");
-
+include("../handlers/categoryHandler.php");
 try{
-    if($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if($_POST['entity'] =="category") {
-            include_once("../handlers/categoryHandler.php");
-            //$Category = new Database();
-            if($_POST['endpoint'] =="addCategory"){                                           
-                $result = $Category->getAllCategory();
-                echo json_encode($result);            
-           
-            }else if($_POST['endpoint'] =="getAllCategory"){                                 
-                    $result = $database->getAllCategory(); 
-                    echo json_encode($result );
-                                    
-            }else {
-                throw new Exception("BBBBBBBBBBBBBBBB",507);
-            }
-
-        } else {
-            throw new Exception("AAAAAAAAAAAAA",501);
-        }
-
-    }
-    else {
-        throw new Exception("Not a valid method",405);
-    }
-    
-}catch(Exception $e){
-   echo json_encode(array("Message"=> $e->getMessage(), "Status"=> $e->getCode()));
+    if($_SERVER['REQUEST_METHOD'] === 'GET') {
+        getCategories();
+}} catch(Exception $e){
+    echo json_encode(array("Message"=> $e->getMessage(), "Status"=> $e->getCode()));
 }
-
-
 ?>
