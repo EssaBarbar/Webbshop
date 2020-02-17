5b025d8cbaebb6997531fd55f3dbe61b82afec9a
Baraabd <bara.abd@yahoo.com>
2020-02-17 18:26:13 +0100

    ategoryClass.php

--- API/classes/categoryClass.php
+++ API/classes/categoryClass.php
@@ -1,43 +1,41 @@
 <?php
-include("./dbClass.php");
+include(realpath(dirname(__FILE__)."/dbClass.php"));
+
+
 class Category {
-    public $categoryId;
-    public $categoryName;
-    
-    function __construct($categoryId, $categoryName) {
-        $this->categoryId = $categoryId;
-        $this->categoryName = $categoryName;        
-    } 
+    public $CategoryId;
+    public $CategoryName;
+    public $db;
 
-    // public function addCategory($categoryId,$categoryName) {
+    function __construct($categoryId = null, $categoryName = null) {
+        $this->CategoryId= $categoryId;
+        $this->CategoryName = $categoryName;
+        $this->db = new Database();
+    }
 
+    public function fetchAll() {
+        $query = "SELECT * FROM Categories";
+        $result = $this->db->runQuery($query);
+        return $result;
+    }
+
+    // public function fetch() {
+    //     $this->db->runQuery("SELECT * FROM.....");
     // }
+
+    // public function insert($query, $values) {
+    //     $this->db->runQuery("INSERT INTO...");
+    // }
+
+    // public function delete() {
+    //     $this->db->runQuery("DELETE FROM... WHERE productID = $this->productId");
+    // }
+
+    // public function update() {
+    //     if ($this->unitPrice)
+    //     $this->db->runQuery("UPDATE... SET Unit");
+    // }
+    
 }
-
-// class Category {
-//     private $conn;
-//     private $table = 'orders';
-
-//     public $CategoryID;
-//     public $CategoryName;
-
-//     public function __construct($db) {
-//         $this->conn = $db;
-//     }
-    
-        
-//     public function read() {
-//         $query = 'SELECT  
-//         CategoryID,
-//         CategoryName
-//         FROM ' . $this->table . ' ';
-//         $stmt = $this->conn->prepare('select * from categories;');
-//         $stmt->execute();
-//         return $stmt;
-//     }
-
-    
-
-// }
 
 ?>
