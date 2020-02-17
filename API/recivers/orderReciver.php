380f8c600bd879dd88a18a41fcd989b1cb8d9f7c
Baraabd <bara.abd@yahoo.com>
2020-02-17 18:42:31 +0100

    Edit code

--- API/recivers/orderReciver.php
+++ API/recivers/orderReciver.php
@@ -1,28 +1,28 @@
 <?php
 
-try {
+// try {
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if ($_POST["entity"] == "enjoy") {
+//     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//         if ($_POST["entity"] == "enjoy") {
 
-            if ($_POST["endpoint"] == "getOrders") {
+//             if ($_POST["endpoint"] == "getOrders") {
 
-                include_once("./orderClass.php");
+//                 include_once("./orderClass.php");
 
-                $database = new Database();
-                $result = $database->createOrder($_POST["newOrder"]);
-                echo json_encode($result);
-            } else {
-                throw new Exception("Not a valid endpoint", 501);
-            }
-        } else {
-            throw new Exception("Not a valid entity method", 501);
-        }
-    } else {
-        throw new Exception("Not valid request method", 405);
-    }
-} catch (Exception $e) {
-    echo json_encode(array("Message" => $e->getMessage(), "Status" => $e->getCode()));
-}
+//                 $database = new Database();
+//                 $result = $database->createOrder($_POST["newOrder"]);
+//                 echo json_encode($result);
+//             } else {
+//                 throw new Exception("Not a valid endpoint", 501);
+//             }
+//         } else {
+//             throw new Exception("Not a valid entity method", 501);
+//         }
+//     } else {
+//         throw new Exception("Not valid request method", 405);
+//     }
+// } catch (Exception $e) {
+//     echo json_encode(array("Message" => $e->getMessage(), "Status" => $e->getCode()));
+// }
 
 ?>
--- API/recivers/productReciver.php
+++ API/recivers/productReciver.php
@@ -1,27 +1,9 @@
 <?php
-try {
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if ($_POST["entity"] == "enjoy") {
-
-            if ($_POST["endpoint"] == "getProducts") {
-
-                include_once("./productClass.php");
-
-                $product = new Product($productID, $productName, $unitPrice, $description, $picture, $categoryID);
-                $result = $product->insert();
-                echo json_encode($result);
-            } else {
-                throw new Exception("Not a valid endpoint", 501);
-            }
-        } else {
-            throw new Exception("Not a valid entity method", 501);
-        }
-    } else {
-        throw new Exception("Not valid request method", 405);
-    }
-} catch (Exception $e) {
-    echo json_encode(array("Message" => $e->getMessage(), "Status" => $e->getCode()));
+include("../handlers/productHandler.php");
+try{
+    if($_SERVER['REQUEST_METHOD'] === 'GET') {
+        getProducts();
+}} catch(Exception $e){
+    echo json_encode(array("Message"=> $e->getMessage(), "Status"=> $e->getCode()));
 }
-
 ?>
