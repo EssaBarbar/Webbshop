5fea46090d946d20d7d90b282ca3f141b469205c
Baraabd <bara.abd@yahoo.com>
2020-02-19 09:42:20 +0100

    .

--- API/classes/productClass.php
+++ API/classes/productClass.php
@@ -1,6 +1,5 @@
 <?php
-//include(realpath(dirname(__FILE__)."/dbClass.php"));
-include("../classes/dbClass.php");
+include(realpath(dirname(__FILE__)."/dbClass.php"));
 
 class Product {
     public $productName;
