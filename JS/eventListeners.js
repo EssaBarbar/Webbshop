ec3b1d65674e0aa31b46f889bc95ea4510a82b33
Baraabd <bara.abd@yahoo.com>
2020-02-19 10:33:47 +0100

    .

--- JS/eventListeners.js
+++ JS/eventListeners.js
@@ -1,20 +1,6 @@
-import { signUpSubmit } from "./signupJS.js"
-import { getCategories } from "./categoryJS.js"
-import { getProducts } from "./productJS.js"
-/* import { getUsers } from "./adminJS.js" */
-import { signUp } from "./loginJS.js"
-
-export function addEventListener() {
-    /* document.getElementById("getUsersbtn").addEventListener("click", getUsers) */
-    // You write here all your event listners
-    const signup = document.getElementById("signUpSubmit")
-    signup && signup.addEventListener("click", signUpSubmit)
-    const button = document.getElementById("signUpbtn")
-    button && button.addEventListener("click", signUp)
-    const categoryAll = document.getElementById("getCategories")
-    categoryAll && categoryAll.addEventListener("click", getCategories)
-    const productAll = document.getElementById("getProducts")
-    productAll && productAll.addEventListener("click", getProducts)
-
-
+// You write here all your event listners
+const signup = document.getElementById("signUpSubmit")
+signup && signup.addEventListener("click", signUpSubmit)
+const button = document.getElementById("getUsersbtn")
+button && button.addEventListener("click", getUsers)
 }
