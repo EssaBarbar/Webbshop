2f2d0ce49fe2b356901f4bd2855dea8296b4eb38
Rasmus1996 <sebbegrymen@gmail.com>
2020-02-19 15:27:42 +0100

    price counter
    price counter without db connection

---
+++ JS/cartJS.js
@@ -0,0 +1,33 @@
+
+
+function addItem() {
+    var pro = document.getElementById('Product').value // lägger till nya items i slutet på array
+    var antal = document.getElementById('Antal').value // lägger till nya items i slutet på array
+    var price = document.getElementById('Price').value // lägger till nya items i slutet på array
+    var total = antal * price
+
+    displayCart(pro, antal, total) // kallar på funktionen "visa-cart".
+}
+function displayCart(pro, antal, total) {
+    var body = document.getElementsByTagName("body")[0]
+
+    var title = document.createElement("h1");
+    title.innerText = pro
+
+    var quan = document.createElement("h2");
+    quan.innerText = antal
+
+    var price = document.createElement("h3");
+    price.innerText = total + " " + "kr";
+
+    body.append(title)
+    body.append(quan)
+    body.append(price)
+}
+
+
+
+
+
+
+
