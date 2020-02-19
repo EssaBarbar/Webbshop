c7b3eb06264c703a8d47500a69ff1416af4fbaaa
Baraabd <bara.abd@yahoo.com>
2020-02-19 09:43:42 +0100

    .

--- API/handlers/productHandler.php
+++ API/handlers/productHandler.php
@@ -1,10 +1,9 @@
 <?php
 include("../classes/productClass.php");
-include("../classes/dbClass.php");
 
 function getProducts() {
-    $Product = new Product(null, null, null, null, null, null);
-    $result = $Product->fetchAll("SELECT * FROM products;");
-    echo json_encode($result);
+    $Product = new Product();
+    $result = $Product->fetchAll();
+    return $result;
 };
 ?>
