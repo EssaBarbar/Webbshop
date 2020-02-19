9c6b55a1725969ee50bf06919d8e1b1724202b64
Baraabd <bara.abd@yahoo.com>
2020-02-19 09:38:25 +0100

    .

--- JS/productJS.js
+++ JS/productJS.js
@@ -1,14 +1,14 @@
 import { makeRequest } from "./requestHandler.js"
 export function getProducts() {
-    // let myData = new FormData();
-    //myData.append("entity", "products");
-    //myData.append("endpoint", "getAllProducts");
-    makeRequest("./API/recivers/productReciver.php", "GET", null, (result) => {
+    let myData = new FormData();
+    myData.append("entity", "products");
+    myData.append("endpoint", "getAllProducts");
+    makeRequest("../api/recivers/productReciver.php", "GET", null, (result) => {
 
-        for (var i = 0; i < result.length; i++) {
+        /* for (var i = 0; i < result.length; i++) {
             console.log(result[i]);
-            //document.getElementById("getProducts").innerHTML = result[i];
-        }
-        //   console.log(result)
+            document.getElementById("categories").innerHTML = result[i];
+        } */
+        console.log(result)
     })
 }
