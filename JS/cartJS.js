ce76e27020c59bd401b3ecfdc245e26a5012b3ab
yanica svensson <sweets86_@hotmail.com>
2020-02-24 02:03:30 +0100

    commity

--- JS/cartJS.js
+++ JS/cartJS.js
@@ -1,16 +1,15 @@
 import { toCart } from "./productJS.js"
 import { count } from "./productJS.js"
 
-var cartList = JSON.parse(sessionStorage.getItem("cart"));
+let cartList = JSON.parse(sessionStorage.getItem("cart"));
 if (cartList && cartList.length) {
     console.log(cartList)
     count(cartList.length)
 
-    /* let totalPrice = 0; */
+    let totalPrice = ""
+
     for (let i = 0; i < cartList.length; i++) {
         let product = cartList[i]
-
-        /* totalPrice += product.price */
 
         let cartProductContainer = document.getElementById('cartProducts')
 
@@ -28,6 +27,8 @@ if (cartList && cartList.length) {
 
         const Price = document.createElement('p')
         Price.innerText = product.Price + " " + "kr"
+
+        console.log(Price)
         cartBox.append(Price)
 
         const removeProduct = document.createElement('button')
@@ -37,15 +38,25 @@ if (cartList && cartList.length) {
         removeProduct.onclick = function () {
             removeItem(this.data);
         }
-
         cartBox.append(removeProduct)
 
         cartProductContainer.append(cartBox)
+    
+        totalPrice += parseFloat(product.Price)
     }
+
+    let checkContainer = document.getElementById("checkBox")
+
+    let priceText = document.createElement("h2");
+    priceText.innerText =
+        "Totalt" + " " + "pris:" + " " + totalPrice + " " + "kr";
+    priceText.classList = "totalText";
+
+    checkContainer.appendChild(priceText);
 }
 
 export function removeItem(index) {
-    var cartList = JSON.parse(sessionStorage.getItem("cart"))
+    let cartList = JSON.parse(sessionStorage.getItem("cart"))
     cartList.splice(index, 1)
     sessionStorage.setItem("cart", JSON.stringify(cartList))
     document.getElementsByTagName("main")[0].innerHTML = ""
