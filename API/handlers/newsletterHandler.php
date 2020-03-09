a531ed289a4fab7b0fc25234e9e2df9bc6d49a9c
Anukhatra <anu.khatra@gmail.com>
2020-03-09 19:11:00 +0100

    newsletter in cart page

--- API/handlers/newsletterHandler.php
+++ API/handlers/newsletterHandler.php
@@ -3,9 +3,9 @@
 
 include("../classes/newsletterClass.php");
 
-function subscribeNews($firstname, $lastname, $email)
+function subscribeNews($userId,$firstname, $lastname, $email)
 {
-    $newsletter = new Newsletter(null,null, $firstname, $lastname, $email);
+    $newsletter = new Newsletter(null,$userId, $firstname, $lastname, $email);
     $result = $newsletter->insert();
     return "done";
     
